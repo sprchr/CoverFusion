6b3fc6a629a25ec9f0fe8c343fe577def4c8ba76
sprchr <sprchrgreat@gmail.com>
2024-12-20 03:35:23 +0530

    Update ResumeBuilder.jsx

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -475,7 +475,11 @@ const ResumeBuilder = () => {
     `;
 
   return (
-    <div>
+     <div style={{
+    backgroundImage: "url('https://i.ibb.co/BZ8xCc0/bgthree.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  }}>
     <p  onClick={()=>{navigate('/')}} className=" hover:cursor-pointer flex justify-center mt-28 font-bold text-[56px] text-blue-500 mb-10">Cover Fusion</p>
     <div className="flex justify-center w-screen">
       <div className="border-2 mt-5 w-[800px] border-gray-800 bg-white">
